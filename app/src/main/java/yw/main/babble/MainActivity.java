b58e93a70a7902ced3c8e6504d3f3e7b34824d3e
Emma Sampugnaro <emmasampugnaro@gmail.com>
2020-02-18 17:48:59 -0500

    Bug not saving new notes

--- app/src/main/java/yw/main/babble/MainActivity.java
+++ app/src/main/java/yw/main/babble/MainActivity.java
@@ -8,22 +8,20 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    EditText editText;
-    String filename = "Note1.txt";
+    ArrayList<NotesBuilder> notesList = new ArrayList<>();
+    private NotesAdapter nAdapter;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,85 +30,57 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        editText = findViewById(R.id.EditText);
-
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                File[] files = getFilesDir().listFiles();
-                filename = "Note" + files.length + ".txt";
-                Log.d("exs", filename);
-                // get a new file name to save
-                Save(filename);
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this, NoteActivity.class);
+                MainActivity.this.startActivity(myIntent);
             }
         });
-        // set these strings better
-        editText.setText(Open(filename));
+
+        listView = findViewById(R.id.notes);
+
+        nAdapter = new NotesAdapter(notesList, this);
+        listView.setAdapter(nAdapter);
+
+        prepareNotes();
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu_main; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    // Change function to work with firebase
-    public void Save(String fileName) {
-        try {
-            OutputStreamWriter out =
-                    new OutputStreamWriter(openFileOutput(fileName, 0));
-            out.write(editText.getText().toString());
-            out.close();
-            Toast.makeText(this, "Note Saved!", Toast.LENGTH_SHORT).show();
-        } catch (Throwable t) {
-            Toast.makeText(this, "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+    private void prepareNotes() {
+        File directory;
+        directory = getFilesDir();
+        File[] files = directory.listFiles();
+        String theFile;
+        for (int f = 1; f <= files.length; f++) {
+            theFile = "Note" + f + ".txt";
+            NotesBuilder note = new NotesBuilder(theFile, Open(theFile));
+            notesList.add(note);
         }
+
     }
 
-    public boolean FileExists(String fname){
-        File file = getBaseContext().getFileStreamPath(fname);
-        return file.exists();
-    }
-
+    // duplicate method - try to reduce or do in thread
     public String Open(String fileName) {
         String content = "";
-        if (FileExists(fileName)) {
-            try {
-                InputStream in = openFileInput(fileName);
-                if ( in != null) {
-                    InputStreamReader tmp = new InputStreamReader( in );
-                    BufferedReader reader = new BufferedReader(tmp);
-                    String str;
-                    StringBuilder buf = new StringBuilder();
-                    while ((str = reader.readLine()) != null) {
-                        buf.append(str + "\n");
-                    } in .close();
-                    content = buf.toString();
-                }
-            } catch (java.io.FileNotFoundException e) {} catch (Throwable t) {
-                Toast.makeText(this, "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+        try {
+            InputStream in = openFileInput(fileName);
+            if ( in != null) {
+                InputStreamReader tmp = new InputStreamReader( in );
+                BufferedReader reader = new BufferedReader(tmp);
+                String str;
+                StringBuilder buf = new StringBuilder();
+                while ((str = reader.readLine()) != null) {
+                    buf.append(str + "\n");
+                } in .close();
+
+                content = buf.toString();
             }
+        } catch (java.io.FileNotFoundException e) {} catch (Throwable t) {
+            Toast.makeText(this, "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
         }
+
         return content;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Intent myIntent = new Intent(MainActivity.this, NoteSelect.class);
-            MainActivity.this.startActivity(myIntent);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
 }
