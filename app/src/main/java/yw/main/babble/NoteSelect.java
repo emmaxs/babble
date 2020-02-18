480b294075131469af358e59cd8c9a916766df68
Emma Sampugnaro <emmasampugnaro@gmail.com>
2020-02-18 17:29:22 -0500

    Finished, but bugs with only saving one note and hiding behind toolbar

---
+++ app/src/main/java/yw/main/babble/NoteSelect.java
@@ -0,0 +1,86 @@
+package yw.main.babble;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class NoteSelect extends AppCompatActivity {
+    ArrayList<NotesBuilder> notesList = new ArrayList<>();
+    private NotesAdapter nAdapter;
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_select);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(NoteSelect.this, MainActivity.class);
+                NoteSelect.this.startActivity(myIntent);
+            }
+        });
+
+        listView = findViewById(R.id.notes);
+
+        nAdapter = new NotesAdapter(notesList, this);
+        listView.setAdapter(nAdapter);
+
+        prepareNotes();
+    }
+
+    private void prepareNotes() {
+        File directory;
+        directory = getFilesDir();
+        File[] files = directory.listFiles();
+        String theFile;
+        for (int f = 1; f <= files.length; f++) {
+            theFile = "Note" + f + ".txt";
+            NotesBuilder note = new NotesBuilder(theFile, Open(theFile));
+            notesList.add(note);
+        }
+
+    }
+
+    // duplicate method - try to reduce or do in thread
+    public String Open(String fileName) {
+        String content = "";
+        try {
+            InputStream in = openFileInput(fileName);
+            if ( in != null) {
+                InputStreamReader tmp = new InputStreamReader( in );
+                BufferedReader reader = new BufferedReader(tmp);
+                String str;
+                StringBuilder buf = new StringBuilder();
+                while ((str = reader.readLine()) != null) {
+                    buf.append(str + "\n");
+                } in .close();
+
+                content = buf.toString();
+            }
+        } catch (java.io.FileNotFoundException e) {} catch (Throwable t) {
+            Toast.makeText(this, "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+        }
+
+        return content;
+    }
+
+}
