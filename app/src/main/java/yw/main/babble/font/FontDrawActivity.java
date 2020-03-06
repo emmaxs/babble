d9dcce8b0db4a60fc7cc71665bdc7e95bec1bfb6
Emma Sampugnaro <emmasampugnaro@gmail.com>
2020-03-05 22:24:33 -0500

    Fonts save in firebase

--- app/src/main/java/yw/main/babble/font/FontDrawActivity.java
+++ app/src/main/java/yw/main/babble/font/FontDrawActivity.java
@@ -22,12 +22,9 @@ import com.flask.colorpicker.builder.ColorPickerClickListener;
 import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
 
 import yw.main.babble.R;
-import yw.main.babble.font.BitmapBuilderAndSaver;
-import yw.main.babble.font.DrawingView;
-import yw.main.babble.font.Glyphs;
 
 public class FontDrawActivity extends Activity {
-    private BitmapBuilderAndSaver builderAndSaver = new BitmapBuilderAndSaver();
+    private BitmapBuilderAndSaver builderAndSaver;
     private DrawingView dv ;
     private TextView letterHint;
     private int currCharIndex = 0;
@@ -36,6 +33,8 @@ public class FontDrawActivity extends Activity {
         super.onCreate(savedInstanceState);
         setTheme(R.style.HeartsTheme);
         setContentView(R.layout.activity_font_draw);
+
+        builderAndSaver = new BitmapBuilderAndSaver();
         builderAndSaver.loadBitmap(this);
         dv = new DrawingView(this);
         dv.post(new Runnable() {
